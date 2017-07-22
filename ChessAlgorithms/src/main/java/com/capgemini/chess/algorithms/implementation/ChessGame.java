ab47366a02b1c47aaa0c6a278a05b0c1bce09471
whiteobelix <pawel.muntowski@gmail.com>
2017-07-22 19:52:23 +0200

    nadal nie działa

---
+++ ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/ChessGame.java
@@ -0,0 +1,28 @@
+package com.capgemini.chess.algorithms.implementation;
+
+import com.capgemini.chess.algorithms.data.Coordinate;
+import com.capgemini.chess.algorithms.data.Move;
+import com.capgemini.chess.algorithms.data.enums.BoardState;
+import com.capgemini.chess.algorithms.data.enums.Color;
+import com.capgemini.chess.algorithms.data.enums.MoveType;
+import com.capgemini.chess.algorithms.data.enums.Piece;
+import com.capgemini.chess.algorithms.data.enums.PieceType;
+import com.capgemini.chess.algorithms.data.generated.Board;
+import com.capgemini.chess.algorithms.implementation.exceptions.InvalidMoveException;
+import com.capgemini.chess.algorithms.implementation.exceptions.KingInCheckException;
+
+public class ChessGame {
+
+	public static void main(String[] args) throws InvalidMoveException, KingInCheckException {
+		// TODO Auto-generated method stub
+		Coordinate sk¹dœ = new Coordinate(0,1);
+		Coordinate dok¹dœ = new Coordinate(0,3);
+		try{
+		BoardManager newGeame = new BoardManager();
+		newGeame.performMove(sk¹dœ, dok¹dœ);
+		}catch (InvalidMoveException e){
+		e.printStackTrace();
+		}
+	}
+
+}
